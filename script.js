291f9cd197b5582a855a7d6ac98e6fd27c9708e2
x-Adrian-x <adrianesteban98@hotmail.com>
2025-12-19 00:34:43 -0500

    Optimiza el codigo de filtrado por ubicación y añade mejoras visuales a la interfaz de empleos

--- script.js
+++ script.js
@@ -12,29 +12,16 @@ contenedor?.addEventListener("click", (event) => {
   }
 });
 
-const select = document.getElementById("filtro-ubicacion");
+const filterLctn = document.getElementById("filtro-ubicacion");
+const ofertas = document.querySelectorAll(".tarjeta-oferta");
 
-select?.addEventListener("change", (event) => {
-  const selectedUbicacion = event.target.value;
-  const ofertas = document.querySelectorAll(".contenedor-ofertas > div");
-
+filterLctn.addEventListener("change", () => {
+  const selected = filterLctn.value;
+  
   ofertas.forEach(oferta => {
     const ubicacionSpan = oferta.querySelector(".ubicacion");
     const ubicacion = ubicacionSpan.textContent;
-
-    if (selectedUbicacion === ubicacion) { 
-      oferta.style.display = "grid";
-    } else {
-      oferta.style.display = "none";
-    }
+    const isShown = selected === "all" || selected === ubicacion;
+    oferta.classList.toggle('is-hidden', !isShown);
   });
 });
-
-// contenedor?.addEventListener("keydown", (event) => {
-//   const elemento = event.target;
-
-//   if (event.key === "Enter") {
-//     window.open("./detalles-empleo.html", "_blank");
-//     // window.location.href = "./detalles-empleo.html";
-//   }
-// });
